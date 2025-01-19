40019b8222218884417927dbfd89307f1d03779a
Исадичева Дарья <116076045+DariaDaria147@users.noreply.github.com>
2025-01-19 22:34:49 +0100

    Completed client

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,29 @@
+# ИСАДИЧЕВА Д.А., ДПИ22-1
+
 import socket
 
-HOST = 'localhost'
-PORT = 6666
+# Конфигурация клиента
+SERVER_HOST = 'localhost'  # Адрес сервера
+SERVER_PORT = 6666         # Порт сервера
 
 while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+    # Ввод команды от пользователя
+    client_request = input('ftp> ')
+
+    # Создание сокета и подключение к серверу
+    client_socket = socket.socket()
+    client_socket.connect((SERVER_HOST, SERVER_PORT))
+
+    # Отправка команды серверу
+    client_socket.send(client_request.encode())
+
+    # Прием и отображение ответа от сервера
+    server_response = client_socket.recv(1024).decode()
+    print(server_response)
+
+    # Закрытие соединения
+    client_socket.close()
+
+    # Выход из цикла при команде 'exit'
+    if client_request == 'exit':
+        break
